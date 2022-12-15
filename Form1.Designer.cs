ab658a12dfab7ad1259a8770168624e2ff91ad9e
miiroo <odnak0@mail.ru>
2022-12-16 00:49:54 +0200

    * Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Baka {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadVulnerabilityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(733, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadVulnerabilityToolStripMenuItem,
+            this.loadFileToolStripMenuItem,
+            this.saveLogsToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadVulnerabilityToolStripMenuItem
+            // 
+            this.loadVulnerabilityToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlText;
+            this.loadVulnerabilityToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.loadVulnerabilityToolStripMenuItem.Name = "loadVulnerabilityToolStripMenuItem";
+            this.loadVulnerabilityToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadVulnerabilityToolStripMenuItem.Text = "Load vulnerability";
+            this.loadVulnerabilityToolStripMenuItem.Click += new System.EventHandler(this.loadVulnerabilityToolStripMenuItem_Click);
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlText;
+            this.loadFileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadFileToolStripMenuItem.Text = "Load file";
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // saveLogsToolStripMenuItem
+            // 
+            this.saveLogsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlText;
+            this.saveLogsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.saveLogsToolStripMenuItem.Name = "saveLogsToolStripMenuItem";
+            this.saveLogsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveLogsToolStripMenuItem.Text = "Save logs";
+            this.saveLogsToolStripMenuItem.Click += new System.EventHandler(this.saveLogsToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(12, 447);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 51);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(175, 351);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // listView2
+            // 
+            this.listView2.AutoArrange = false;
+            this.listView2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.LabelWrap = false;
+            this.listView2.Location = new System.Drawing.Point(217, 51);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(492, 351);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Vulnerabilities";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(214, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Logs";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(217, 418);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(492, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(217, 447);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(492, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save logs";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.saveLogsToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Location = new System.Drawing.Point(12, 478);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Continue";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(217, 476);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(492, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(733, 513);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Detector";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadVulnerabilityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveLogsToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Baka {
+    public partial class Form1 : Form {
+
+        private List<string> vulnerability = new List<string>();
+        private List<string> logStrings = new List<string>();
+        private List<string> logs = new List<string>();
+        private List<Task> tasks = new List<Task>();
+        private bool isPaused = false;
+        private bool foundIssue = false;
+        private bool isFinished = false;
+        private int completed = 0;
+        private int completedForBar = 0;
+        public Form1() {
+            InitializeComponent();
+        }
+#region Work_with_Files
+        private void loadVulnerabilityToolStripMenuItem_Click(object sender, EventArgs e) {
+            var filePath = string.Empty;
+            listView1.Items.Clear();
+            vulnerability.Clear();
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream)) {
+                        MessageBox.Show("File "+ openFileDialog.FileName + " is loaded");
+                        while (!reader.EndOfStream)
+                            vulnerability.Add(reader.ReadLine());
+                    }
+                }
+            }
+
+            for (int i =0; i<vulnerability.Count-1; i++) { 
+                listView1.Items.Add(vulnerability[i]);
+            }
+        }
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e) {
+            var filePath = string.Empty;
+            logStrings.Clear();
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream)) {
+                        MessageBox.Show("File " + openFileDialog.FileName + " is loaded");
+                        while (!reader.EndOfStream)
+                            logStrings.Add(reader.ReadLine());
+                    }
+
+                }
+            }
+        }
+        private void saveLogsToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog()) {
+                StreamWriter myStream;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                    if ((myStream = new StreamWriter(saveFileDialog1.FileName)) != null) {
+                        foreach (var log in logs) {
+                            myStream.WriteLine(log);
+                        }
+                        myStream.Close();
+                    }
+                }
+            }
+
+        }
+#endregion
+
+        //Start search
+        private async void button1_Click(object sender, EventArgs e) {
+            //Check for empty files
+            if (vulnerability.Count <= 0 || logStrings.Count <= 0) {
+                MessageBox.Show("Please add vulnerabilities and file for check");
+                return;
+            }
+            //Prepearing stuff before start searching
+            if (tasks.Count != 0) tasks.Clear();
+
+            isFinished = false;
+            completed = 0;
+            completedForBar = 0;
+
+            Task.Run(async () => onLogUpdate());
+            Task.Run(async () => barUpdate());
+
+            await Task.Run(() => {
+
+                logStrings.ForEach(x => {
+                        tasks.Add(Task.Run(async () => {
+                            await Task.Delay(100);
+                            while (!isPaused) {
+                                vulnerability.ForEach(y => { 
+                                    if (x.Contains(y)) {
+                                        foundIssue = true;
+                                        logs.Add("Vulnerability: " + y + " found in: " + x);
+                                    }
+                                });
+                                completedForBar++;
+                                break;
+                            }
+                        }));
+                });
+                Task.WaitAll(tasks.ToArray());
+                if (!foundIssue) {
+                    logs.Add("No issue found");
+                }
+                Task.Delay(100);
+            });
+            isFinished = true;
+            logStrings.Clear();
+            ShowAll();
+            logs.Clear();
+        }
+
+        //Bar update
+        private async void barUpdate() {
+            while (!isFinished) {
+                UpdateBar();
+            }
+        }
+
+ 
+        //Log updates
+        private async void onLogUpdate() {
+            while (!isFinished) {
+                if (completed < logs.Count) {
+                    LogUpdate();
+                    completed = logs.Count;
+                }
+            }
+        }
+
+#region UI_update
+
+        private void ShowAll() {
+            foreach (var log in logs) 
+                listView2.Items.Add(log);
+        }
+        private void LogUpdate() {
+            if (this.InvokeRequired && this != null) {
+                this.Invoke((Action)LogUpdate);
+            }
+            else {
+                listView2.Items.Add(logs.Last());
+            }
+        }
+        private void UpdateBar() {
+            if (this.InvokeRequired && this != null) {
+                this.Invoke((Action)UpdateBar);
+            }
+            else {
+                if (logStrings.Count > 0)
+                    progressBar1.Value = (int)Math.Round(((float)(completedForBar) / (logStrings.Count)) * 100);
+            }
+        }
+#endregion
+        //Set pause
+        private void button2_Click(object sender, EventArgs e) => isPaused = true;
+
+        //Set unpause
+        private void button4_Click(object sender, EventArgs e) => isPaused = false;
+
+        //Clear logs
+        private void button5_Click(object sender, EventArgs e) => listView2.Items.Clear();
+    }
+}
